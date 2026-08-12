ed75c26e8d6821ced21f8accfe43a0e8cb1e0a63
nicolas838 <robayonicolas123@gmail.com>
2026-08-12 12:03:40 -0600

    Add sitemap route to serve XML content
    Added a new route for sitemap.xml to serve XML content.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    contenido = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://pagina-web-mv-electromedicina.onrender.com/</loc>
+    </url>
+</urlset>"""
+
+    return Response(contenido, mimetype="application/xml")
 
 
 @app.route('/solicitud', methods=['GET', 'POST'])
