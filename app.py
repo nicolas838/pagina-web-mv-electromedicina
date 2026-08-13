3c3e5c241ab24f61b3ef78c54bb9848e42350ae5
nicolas838 <robayonicolas123@gmail.com>
2026-08-13 06:52:10 -0600

    Fix string quotes and clean up sitemap response

--- app.py
+++ app.py
@@ -3,51 +3,43 @@ from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def inicio():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/sitemap.xml')
+
+@app.route("/sitemap.xml")
 def sitemap():
-    contenido = """<?xml version="1.0" encoding="UTF-8"?>
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>https://pagina-web-mv-electromedicina.onrender.com/</loc>
     </url>
-</urlset>"""
+</urlset>
+"""
 
-    
-@app.route('/robots.txt')
-def robots():
-    return Response(
-        "User-agent: *\n"
-        "Allow: /\n"
-        "\n"
-        "Sitemap: https://pagina-web-mv-electromedicina.onrender.com/sitemap.xml\n",
-        mimetype="text/plain"
-    )
-
-    return Response(contenido, mimetype="application/xml")
+    return Response(xml, content_type="application/xml; charset=utf-8")
 
 
-@app.route('/solicitud', methods=['GET', 'POST'])
+@app.route("/solicitud", methods=["GET", "POST"])
 def solicitud():
 
-    if request.method == 'POST':
-
-        cliente = request.form['cliente']
-        empresa = request.form['empresa']
-        correo = request.form['correo']
-        telefono = request.form['telefono']
-        equipo = request.form['equipo']
-        marca = request.form['marca']
-        serie = request.form['serie']
-        falla = request.form['falla']
-        prioridad = request.form['prioridad']
+    if request.method == "POST":
+        cliente = request.form.get("cliente")
+        empresa = request.form.get("empresa")
+        correo = request.form.get("correo")
+        telefono = request.form.get("telefono")
+        equipo = request.form.get("equipo")
+        marca = request.form.get("marca")
+        serie = request.form.get("serie")
+        falla = request.form.get("falla")
+        prioridad = request.form.get("prioridad")
 
         print("===== NUEVA SOLICITUD =====")
         print("Cliente:", cliente)
         print("Empresa:", empresa)
+        print("Correo:", correo)
+        print("Telefono:", telefono)
         print("Equipo:", equipo)
         print("Marca:", marca)
         print("Serie:", serie)
@@ -59,5 +51,6 @@ def solicitud():
 
 if __name__ == "__main__":
     import os
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
